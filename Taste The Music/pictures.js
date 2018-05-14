c564e01f2728552f8bab648fea4d875a05bb39e2
MihaelaGadzhalova707 <mihaelagadzhalova@gmail.com>
2018-05-15 00:28:00 +0300

    keyboards

--- Taste The Music/pictures.js
+++ Taste The Music/pictures.js
@@ -1,16 +1,16 @@
 <script>
-					// Get the modal
+					var myIndex = 0;
+					carousel();
 
-						function enlargeImage() {
-  document.getElementById("img01").src = this.src;
-  document.getElementById("caption").innerHTML = this.alt;
-  document.getElementById('myModal').style.display = "block";
-}
-
-(function() {
-  var images = document.getElementsByClassName("myImg");
-  for (i = 0; i < images.length; i++) {
-    images[i].onclick = enlargeImage;
-  }
-})();
-</script>
+					function carousel() {
+						var i;
+						var x = document.getElementsByClassName("mySlides");
+						for (i = 0; i < x.length; i++) {
+							x[i].style.display = "none";  
+						}
+						myIndex++;
+						if (myIndex > x.length) {myIndex = 1}    
+						x[myIndex-1].style.display = "block";  
+						setTimeout(carousel, 2000); // Change image every 2 seconds
+					}
+				</script>
